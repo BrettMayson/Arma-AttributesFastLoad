442e202ac19fc3105f9c4cf06d24832edec527dd
BrettMayson <brett@mayson.io>
2023-08-20 16:47:28 -0600

    initial

---
+++ addons/main/config.cpp
@@ -0,0 +1,14 @@
+class CfgPatches {
+    class 3den_attributes_fix {
+        name = "3den_attributes_fix";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 2.10;
+        requiredAddons[] = { "A3_Data_F_AoW_Loadorder" };
+        authors[] = {"Brett Mayson"};
+        author = "Brett Mayson";
+        version = 1.0;
+        versionStr = "1.0.1";
+        versionAr[] = {1,0,1};
+    };
+};
